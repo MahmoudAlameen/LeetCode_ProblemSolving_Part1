72428fada32408f35581e0ec4a714fe46a4727d1
MahmoudAlameen <alameen2591999@gmail.com>
2025-03-10 01:41:47 +0200

    remove duplication

---
+++ Remove_Duplication/Duplication.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_ProblemSolving_FirstWeek.Remove_Duplication
+{
+    public class Duplication
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            int incomingPosition = 1;
+            int value = nums[0];
+            int occurenceCount = 1;
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] == value && occurenceCount < 2)
+                {
+                    nums[incomingPosition] = nums[i];
+                    occurenceCount++;
+                    incomingPosition++;
+
+                }
+                if (nums[i] != value)
+                {
+                    nums[incomingPosition] = nums[i];
+                    occurenceCount = 1;
+                    incomingPosition++;
+                    value = nums[i];
+                }
+            }
+            return incomingPosition;
+        }
+    }
+}
