fc84928893730f66ff9d830a9e3598d7172410a0
MahmoudAlameen <alameen2591999@gmail.com>
2025-03-15 15:22:44 +0200

    Remove DUplication from sorted linked list

---
+++ LinkedList/RemoveDuplicates.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_ProblemSolving_FirstWeek.LinkedList
+{
+    public class RemoveDuplicates
+    {
+        public ListNode DeleteDuplicates(ListNode head)
+        {
+            ListNode current = head;
+
+            ListNode Prev = head;
+            int? value = null;
+
+            while(current != null)
+            {
+                if (current.val == value)
+                {
+                    Prev.next = current.next;
+                }
+                else
+                {
+                    value = current.val;
+                    Prev = current;
+                }
+                current = current.next;
+            }
+            return head;
+        }
+    }
+}
